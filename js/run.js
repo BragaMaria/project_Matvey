886cf59049a500ac86105bc0ddc8c186cade381c
BragaMaria <brag4.m@yandex.com>
2023-10-12 16:00:06 +0500

    just commit

---
+++ js/run.js
@@ -0,0 +1,12 @@
+const swiper = new Swiper(".swiper", {
+  // Optional parameters
+  direction: "horizontal",
+  slidesPerView: 4,
+  spaceBeetwen:50,
+
+  // If we need pagination
+  pagination: {
+    el: ".swiper-pagination",
+    clickable: true,
+  },
+});
